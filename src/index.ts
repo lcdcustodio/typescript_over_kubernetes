21278ff40b077c4ccd7b19b85347dee65daf6eda
Leonardo Dorneles <leonardo.dorneles@meta.com.br>
2023-04-19 11:30:09 -0300

    intro Redis

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,44 @@
 import express from 'express';
+//import redis, { RedisClient } from 'redis';
+import redis, { createClient } from 'redis'
+//import { RedisClient } from "../node_modules/@redis";
+
+// Create a Redis client
+/*
+const client = redis.createClient({
+  host: 'redis-headless', // Redis host
+  port: 6379, // Redis port
+});
+*/
+
+
+
+// Connect to Redis
+//client.on('connect', () => {
+//  console.log('Connected to Redis');
+//});
+
+//function get value from redis
+async function getRedis(){
+
+  const client = createClient({ url: 'redis://redis-headless:6379' })
+  client.connect();
+  client.on("error", (error) => {
+    console.error(error);
+  });
+
+  const result = await client.set("key", "value");
+  console.log(`result: ${result}`);
+  
+  const result2 = await client.get("key");
+  console.log(`result2: ${result2}`);  
+
+  //const value = await client.get('name');
+  return result2
+
+}
+// Set a key-value pair
+//client.set('name', 'John')
 
 const app = express();
 app.listen(3000, () => {
@@ -11,4 +51,13 @@ app.get('/', (req, res) => {
     res.send('Hello World, from express');
 });
 
+
+app.get('/get', async (req, res) => {
+  const response  = await getRedis()
+  console.log('Hello World, from express: ' + response);
+  res.send('Redis Value: ' + response);
+});
+
+
 app.listen(() => console.log(`Hello world app listening on port`))
+
